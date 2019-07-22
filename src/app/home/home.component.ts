c62997f3e435ae9b73e0f46022a5b61a92eca64f
MOHD SALIM <mohd.salim@soprasteria.com>
2019-07-22 10:38:04 +0530

    Navigating Programatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log("HomeComponent loaded...");
   }
 
+  onLoadServers() {
+    // some complex calculation
+    // this.router.navigate(['/servers/salim']);
+    this.router.navigate(['/servers', 'salim']);
+  }
 }
