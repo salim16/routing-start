1174dcad16f995ea47b0f7859a5b84b2bcaf9622
MOHD SALIM <mohd.salim@soprasteria.com>
2019-07-22 13:40:23 +0530

    Important note about Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+ 
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,12 +20,21 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    // only the subscription code will run on ng on init
+    // since subscribe is an observable, it would run only when params change
+    // one important thing is even if the component is destroyed and re-initialiized, the subscription is never destroyed
+    // threfore we destroy it explicity in ngOnDestroy method 
     this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    // throw new Error("Method not implemented.");
+    this.paramsSubscription.unsubscribe();
   }
 
 }
